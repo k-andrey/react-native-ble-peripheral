5ff15d7fbec384f1cab18e5352c1f92d4a9693f4
Andrey K <andryxa.2011@gmail.com>
2022-03-02 10:00:11 +0300

    Fix gradle version

--- android/src/main/java/com/himelbrand/ble/peripheral/RNBLEPackage.java
+++ android/src/main/java/com/himelbrand/ble/peripheral/RNBLEPackage.java
@@ -6,22 +6,25 @@ import java.util.List;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.himelbrand.ble.peripheral.RNBLEModule;
 
 public class RNBLEPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<NativeModule>();
+        modules.add(new RNBLEModule(reactContext));
+        return modules;
+    }
 
-  @Override
-  public List<NativeModule> createNativeModules(
-  ReactApplicationContext reactContext) {
-    List<NativeModule> modules = new ArrayList<NativeModule>();
-    modules.add(new RNBLEModule(reactContext));
-    return modules;
-  }
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
 
-  @Override
-  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-    return Collections.emptyList();
-  }
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 }
