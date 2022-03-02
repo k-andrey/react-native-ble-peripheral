5d0661cb8b8dab451fd57c0682f5d89c1b476b32
Andrey K <andryxa.2011@gmail.com>
2022-03-02 12:59:37 +0300

    Fix

--- android/src/main/java/com/himelbrand/ble/peripheral/RNBLEPackage.java
+++ android/src/main/java/com/himelbrand/ble/peripheral/RNBLEPackage.java
@@ -22,9 +22,4 @@ public class RNBLEPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
 }
